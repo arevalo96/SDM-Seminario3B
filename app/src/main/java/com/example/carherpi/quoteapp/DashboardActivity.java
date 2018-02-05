d6fb6f3f604938bca067ddd75a6fae52bfc563b4
CARLOS HERRERA PIEDRA <carherpi@alumno.upv.es>
2018-02-05 16:35:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/carherpi/quoteapp/DashboardActivity.java
@@ -0,0 +1,39 @@
+package com.example.carherpi.quoteapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        ((Button)findViewById(R.id.getquotes)).setOnClickListener(listener);
+        ((Button)findViewById(R.id.)).setOnClickListener(listener);
+        ((Button)findViewById(R.id.)).setOnClickListener(listener);
+        ((Button)findViewById(R.id.)).setOnClickListener(listener);
+
+    }
+
+    public void click() {
+        Log.i("TAG","FUNCIONAAA");
+
+    }
+
+    public View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case(R.id.getquotes)
+            }
+
+        }
+    }
+
+
+}
