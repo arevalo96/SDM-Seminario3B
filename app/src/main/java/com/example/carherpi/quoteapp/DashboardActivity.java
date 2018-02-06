3ebcf976bb37bed71752b4385821b38dfe44cbb1
arevalo96 <32680792+arevalo96@users.noreply.github.com>
2018-02-06 19:57:25 +0100

    Ventana about terminada

--- app/src/main/java/com/example/carherpi/quoteapp/DashboardActivity.java
+++ app/src/main/java/com/example/carherpi/quoteapp/DashboardActivity.java
@@ -1,5 +1,6 @@
 package com.example.carherpi.quoteapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,23 +14,29 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-     //  ((Button)findViewById(R.id.getquotes)).setOnClickListener(listener);
-     //   ((Button)findViewById(R.id.)).setOnClickListener(listener);
-     //   ((Button)findViewById(R.id.)).setOnClickListener(listener);
-     //   ((Button)findViewById(R.id.)).setOnClickListener(listener);
+        //((Button)findViewById(R.id.getquotes)).setOnClickListener(listener);
+        //((Button)findViewById(R.id.favoritequotes)).setOnClickListener(listener);
+        //((Button)findViewById(R.id.settings)).setOnClickListener(listener);
+        //((Button)findViewById(R.id.about)).setOnClickListener(listener);
 
     }
 
-   /* public void click() {
-        Log.i("TAG","FUNCIONAAA");
-
+    public void clickAbout(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 
+
+
+    /*
     public View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch(v.getId()){
-                case(R.id.getquotes)
+                case(R.id.getquotes): Intent startNewActivity = new Intent(this, AboutActivity.class);
+                case(R.id.favoritequotes) //Intent startNewActivity = new Intent(this,X.class);
+                case(R.id.getquotes) //Intent startNewActivity = new Intent(this,X.class);
+                case(R.id.getquotes) //Intent startNewActivity = new Intent(this,X.class);
             }
 
         }
